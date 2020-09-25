9c75d711578b82556ead032e5da010db780847b8
keeks5456 <Akikogreen08@gmail.com>
2020-09-25 09:52:35 -0700

    request table

--- db/migrate/20200925002516_create_requests.rb
+++ db/migrate/20200925002516_create_requests.rb
@@ -1,10 +1,12 @@
 class CreateRequests < ActiveRecord::Migration[6.0]
   def change
     create_table :requests do |t|
-      t.string :status 
-      t.text :message
-      t.references :user, null: false, foreign_key: true
-      t.references :service, null: false, foreign_key: true
+      t.integer "request_id"
+      t.integer "provider_id"
+      t.string "status", default: "pending"
+      t.text "message"
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
 
       t.timestamps
     end
