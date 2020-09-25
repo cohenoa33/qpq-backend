f3bc74ebafdf0bf51639925d8d4c7a0f30c09e97
bdb2381 <brad.beggs@gmail.com>
2020-09-25 12:53:18 -0700

    draft of all seed data. needs reviewed

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,65 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_09_25_161627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "requests", force: :cascade do |t|
+    t.string "status"
+    t.text "message"
+    t.bigint "user_id", null: false
+    t.bigint "service_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["service_id"], name: "index_requests_on_service_id"
+    t.index ["user_id"], name: "index_requests_on_user_id"
+  end
+
+  create_table "service_categories", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "service_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_service_categories_on_category_id"
+    t.index ["service_id"], name: "index_service_categories_on_service_id"
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "name"
+    t.boolean "isService"
+    t.text "offeringDescription"
+    t.text "exchangeDescription"
+    t.string "img_url"
+    t.integer "value"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "street"
+    t.string "city"
+    t.string "state"
+    t.integer "zipcode"
+    t.integer "birth_year"
+    t.string "img_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
+    t.string "password_digest"
+  end
+
+  add_foreign_key "requests", "services"
+  add_foreign_key "requests", "users"
+  add_foreign_key "service_categories", "categories"
+  add_foreign_key "service_categories", "services"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,72 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+
+
+# Faker::Commerce.department    "Grocery, Health & Beauty"
+# Faker::Commerce.product_name "Practical Granite Shirt"
+
+# Faker::Lorem.sentence 
+# Faker::Lorem.paragraph
+
+# join tables are last
+
+Users.destroy_all
+Services.destroy_all
+Categories.destroy_all
+Service_categories.destroy_all
+Requests.destroy_all
+
+30.times do
+	User.create({
+  first_name:  Faker::Name.first_name, 
+  last_name:  Faker::Name.last_name,
+  street:Faker::Address.street_name,
+  city:Faker:: Address.city,
+  state: Faker::Address.state_abbr,
+  zipcode: Faker::Address.zip_code,
+  birth_year: Faker::Date.birthday(min_age: 18, max_age: 65),
+  img_url: Faker::Avatar.image,
+  email: Faker::Internet.email,
+  password:	Faker::Internet.password,
+  password_confirmation:
+})
+end
+
+30.times do
+  Service.create( 
+    name: Faker::Commerce.product_name, 
+    isService:
+    offeringDescription: Faker::Lorem.sentence, 
+    exchangeDescription: Faker::Lorem.paragraph,
+    img_url: Faker::LoremFlickr.image,
+    value:
+)
+
+20.times do
+  Categories.create({
+    name: Faker::Commerce.department 
+
+  })
+
+# this needs something different for increment
+# X.times do    
+  # Service_categories.create({
+  #   category_id:
+  #   service_id:
+  # })
+
+  # X.times do 
+  # Request.create({
+  #   status:
+  #   message: Faker::Lorem.paragraph,
+  #   user_id:
+  #   service_id:
+  # })
+
+
+  puts "Data is seeded"
