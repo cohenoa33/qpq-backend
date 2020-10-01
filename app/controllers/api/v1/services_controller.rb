86e81af94c18fb2f67096d699a4905d08f236902
bdb2381 <brad.beggs@gmail.com>
2020-09-30 18:59:50 -0700

    added create action to skip before action in ServicesController

--- app/controllers/api/v1/services_controller.rb
+++ app/controllers/api/v1/services_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ServicesController < ApplicationController
-  skip_before_action :authorized, only: [:index, :show]
+  skip_before_action :authorized, only: [:index, :show, :create]
 
   def index
     services = Service.all
