89ac68c3795c64f28e90410e604df2b844976dca
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-10-01 20:26:15 -0700

    update dependent destroy for class User

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,6 @@
 class Service < ApplicationRecord
   has_many :service_categories, dependent: :destroy
-  has_many :categories, through: :service_categories
+  has_many :categories, through: :service_categories, dependent: :destroy
   belongs_to :user
 
 
