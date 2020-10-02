5d9844d5e99c3c1094792c633c43997a80fb2cbe
Nathan <chung.nathanh@gmail.com>
2020-10-02 09:06:50 -0700

    updates user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :img_url, :street, :city, :state, :zipcode
+  attributes :id, :first_name, :last_name, :email, :img_url, :street, :city, :state, :zipcode, :latitude, :longitude
   
 
   has_many :services
