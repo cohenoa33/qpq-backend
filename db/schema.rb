52d8629ed9abcd6ee95121798dd1afb3af8387d9
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-10-26 10:48:00 -0700

    first heroku commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_152015) do
+ActiveRecord::Schema.define(version: 2020_10_01_011355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_09_29_152015) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.string "slug"
     t.index ["user_id"], name: "index_services_on_user_id"
   end
 
