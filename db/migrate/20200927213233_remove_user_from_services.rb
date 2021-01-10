74d8342148da3e8eb373e14b1648ac234ab17e11
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2021-01-10 04:45:42 -0800

    more hanedling migrations errors

--- db/migrate/20200927213233_remove_user_from_services.rb
+++ db/migrate/20200927213233_remove_user_from_services.rb
@@ -1,5 +1,6 @@
 class RemoveUserFromServices < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :services, :user_id, :integer
-  end
+  # def change
+  #   remove_column :services, :user_id, :integer
+  # end
 end
+
