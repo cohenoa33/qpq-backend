4d499b7989db8d735efc7f09b9e1ff3b0b3e044a
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2021-01-10 04:42:09 -0800

    hanedling migrations errors

--- db/migrate/20200925165541_add_columns_to_requests.rb
+++ db/migrate/20200925165541_add_columns_to_requests.rb
@@ -1,6 +1,7 @@
 class AddColumnsToRequests < ActiveRecord::Migration[6.0]
-  def change
-    add_column :requests, :provider_id, :integer
-    add_column :requests, :requester_id, :integer
-  end
+  # def change
+  #   add_column :requests, :provider_id, :integer
+  #   add_column :requests, :requester_id, :integer
+  # end
 end
+
