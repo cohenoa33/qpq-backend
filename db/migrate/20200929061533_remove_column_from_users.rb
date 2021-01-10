a841dc14a1baba1d1358ea649fd920534f54a7b3
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2021-01-10 05:11:12 -0800

    more hanedling migrations errors

--- db/migrate/20200929061533_remove_column_from_users.rb
+++ db/migrate/20200929061533_remove_column_from_users.rb
@@ -1,5 +1,6 @@
 class RemoveColumnFromUsers < ActiveRecord::Migration[6.0]
-  def change
-    remove_index :email, name: "index_users_on_email"
-  end
+  # 01/10/2021
+  # def change
+  #   remove_index :email, name: "index_users_on_email"
+  # end
 end
--- db/migrate/20200929152015_drop_jwt_denylists_table.rb
+++ db/migrate/20200929152015_drop_jwt_denylists_table.rb
@@ -1,5 +1,6 @@
 class DropJwtDenylistsTable < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :jwt_denylists
-  end
+  # 01/10/2021
+  # def change
+  #   drop_table :jwt_denylists
+  # end
 end
